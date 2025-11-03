ca039a5382572aab1e0b6cbcd6ccf15a59a62ba2
ElBuenTocino <paumm8@gmail.com>
2025-11-03 16:39:23 +0100

    coses

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 local Vector = Vector or require "lib/vector"
 local Actor = Actor or require "lib/actor"
-local Player = Player or require "player"
+local Player = Player or require "src/player"
 -- local Spawner = Spawner or require "src/spawner"
 
 math.randomseed(os.time())
@@ -19,7 +19,7 @@ function love.update(dt)
     v:update(dt)
   end
 end
---fjdfcws
+
 function love.draw()
   love.graphics.print("Number astro eliminated: "..tostring(score),10,10)
   for k, v in ipairs(actorList) do
