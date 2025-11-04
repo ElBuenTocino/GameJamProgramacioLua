32243a01734dfde0b3cd3fdadbf6970979ae3f58
serar3 <sergio.alsa33@gmail.com>
2025-11-04 12:05:31 +0100

    music type shit

---
+++ src/data.lua
@@ -0,0 +1,11 @@
+sounds = 
+{
+    menuMusic = nil,
+    mainMusic = nil,
+    hurtSound = nil,
+    eatSound = nil,
+    enemySound = nil,
+    focoSound = nil,
+    dieSound = nil
+    -- love.audio.play(sound)
+}
